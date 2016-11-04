9a392af69727925ad4c285df0cfd60cc3821161e
Matthieu34400 <matthieu.verquera@gmail.com>
2016-11-04 14:49:31 +0100

    recup et changement niveau contact

--- web/php/send-contact.php
+++ web/php/send-contact.php
@@ -1,24 +1,29 @@
 <?php
 
-	$nom = @trim(stripslashes($_POST['nom'])); 
-	$email = @trim(stripslashes($_POST['email']));
-	$sujet = @trim(stripslashes($_POST['sujet']));
-	$lieux = @trim(stripslashes($_POST['lieux']));
-	$message = @trim(stripslashes($_POST['message'])); 
+if (!array_key_exists('lieu', $_POST) || $_POST['lieu'] == 0) {
+    $errors['lieu'] = "Veuillez choisir un lieu";
+}
 
-	$email_from = $email;
-	$email_to = 'jonathanfournival@gmail.com';//remplacer avec votre email
 
-	$body = 'Nom: ' . $nom . "\n\n" . 'Email: ' . $email . "\n\n" . 'Sujet: ' . $sujet . "\n\n" . 'Lieux: ' . $lieux . "\n\n" . 'Message: ' . $message;
+$nom = @trim(stripslashes($_POST['nom']));
+$email = @trim(stripslashes($_POST['email']));
+$sujet = @trim(stripslashes($_POST['sujet']));
+$message = @trim(stripslashes($_POST['message']));
 
-	$success = @mail($email_to, $body, 'Nom: ' . $nom . "\n\n" . 'Email: ' . $email . "\n\n" . 'Sujet: ' . $sujet . "\n\n" . 'Lieux: ' . $lieux . "\n\n" . 'Message: ' . $message);
-	
+
+
+$email_from = $email;
+$email_to = 'jonathanfournival@gmail.com'; //remplacer avec votre email
+
+$body = 'Nom: ' . $nom . "\n\n" . 'Email: ' . $email . "\n\n" . 'Sujet: ' . $sujet . "\n\n" . 'Lieux: ' . $lieux . "\n\n" . 'Message: ' . $message;
+
+$success = @mail($email_to, $body, 'Nom: ' . $nom . "\n\n" . 'Email: ' . $email . "\n\n" . 'Sujet: ' . $sujet . "\n\n" . 'Lieux: ' . $lieux . "\n\n" . 'Message: ' . $message);
 ?>
 
 <!DOCTYPE HTML>
 <html lang="fr">
-<head>
-	<script>alert("Nous vous remercions de nous avoir contacté. Nous vous contacterons au plus vite.");</script>
-	<meta HTTP-EQUIV="REFRESH" content="0; url=http://localhost/auDelaDesPensees/web/app_dev.php/contact"> 
-	<meta charset="utf-8"> 
-</head>
+    <head>
+        <script>alert("Nous vous remercions de nous avoir contacté. Nous vous contacterons au plus vite.");</script>
+        <meta HTTP-EQUIV="REFRESH" content="0; url=http://localhost/auDelaDesPensees/web/app_dev.php/contact"> 
+        <meta charset="utf-8"> 
+    </head>
